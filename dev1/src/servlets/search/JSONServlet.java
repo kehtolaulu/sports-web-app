042202a34be7831a26adde643639ef5bc81c9a18
kehtolaulu <emilia270627@gmail.com>
2018-10-20 17:28:14 +0300

    add servlets mapping rest

--- dev1/src/servlets/search/JSONServlet.java
+++ dev1/src/servlets/search/JSONServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+
 @WebServlet(name = "JSONServlet")
 public class JSONServlet extends HttpServlet {
     private MatchService matchService;
